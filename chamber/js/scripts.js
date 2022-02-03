a0d888d3b712f019aa64c4aed68da80a27d8b396
jessicabreedlove <jessicabreedlove@gmail.com>
2022-02-02 21:30:49 -0700

    Header done!

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -5,3 +5,21 @@ function toggleMenu() {
 
 const hamBtn = document.getElementById("hamburgerBtn");
 hamBtn.onclick = toggleMenu;
+
+const lastmod = document.querySelector("#lastmod");
+lastmod.textContent = document.lastModified;
+
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
