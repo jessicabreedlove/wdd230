66616dce606ffa77d5a72a6a7dfc47dddf00c02d
jessicabreedlove <jessicabreedlove@gmail.com>
2022-02-05 21:03:57 -0700

    added check for empty!

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,86 +1,29 @@
-// const listsize = document.querySelector("#listsize");
-// const buildButton = document.querySelector("");
-
-// buildButton.addEventListener("click");
-
-// function clicked() {
-//   //get list size
-//   let size = Number(listsize.value);
-//   //build html
-//   for (let i = 1; i <= size; i++) {
-//     let li = document.createElement("li");
-//     li.textContent = "Item ${i}";
-
-//   }
-//   //output to the DOM
-// }
-
-//button.addEventListener('click', function(){})
-
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
-button.addEventListener("click", clicked);
+button.addEventListener("click", function () {
+  if (input.value == "") {
+    alert("Invalid input.  You must enter a Book and Chapter.");
+  } else {
+    let chapter = input.value;
+    input.value = "";
 
-function clicked() {
-  let chapter = input.value;
-  input.value = "";
+    const listItem = document.createElement("li");
+    const scrip = document.createElement("span");
+    const removeBtn = document.createElement("button");
 
-  const listItem = document.createElement("li");
-  const scrip = document.createElement("span");
-  const removeBtn = document.createElement("button");
+    listItem.appendChild(scrip);
+    scrip.textContent = chapter;
+    listItem.appendChild(removeBtn);
+    removeBtn.textContent = "❌";
+    list.appendChild(listItem);
 
-  listItem.appendChild(scrip);
-  scrip.textContent = chapter;
-  listItem.appendChild(removeBtn);
-  removeBtn.textContent = "X";
-  list.appendChild(listItem);
+    removeBtn.addEventListener("click", remove);
 
-  removeBtn.addEventListener("click", remove);
-
-  function remove() {
-    list.removeChild(listItem);
+    function remove() {
+      list.removeChild(listItem);
+    }
+    input.focus();
   }
-  input.focus();
-}
-
-// do {
-//   const listItem = document.createElement("li");
-//   const scrip = document.createElement("span");
-//   const removeBtn = document.createElement("button");
-
-//   listItem.appendChild(scrip);
-//   scrip.textContent = chapter;
-//   listItem.appendChild(removeBtn);
-//   removeBtn.textContent = "X";
-//   list.appendChild(listItem);
-
-//   removeBtn.addEventListener("click", remove);
-
-//   function remove() {
-//     list.removeChild(listItem);
-//   }
-// } while (input.value !== "");
-
-// for (let i = 0; i < chapter.length; i++)
-//   if (input !== "") {
-//     const listItem = document.createElement("li");
-//     const scrip = document.createElement("span");
-//     const removeBtn = document.createElement("button");
-
-//     listItem.appendChild(scrip);
-//     scrip.textContent = chapter;
-//     listItem.appendChild(removeBtn);
-//     removeBtn.textContent = "X";
-//     list.appendChild(listItem);
-
-//     removeBtn.addEventListener("click", remove);
-
-//     function remove() {
-//       list.removeChild(listItem);
-//     }
-//   }
-
-// input.focus();
-// }
+});
