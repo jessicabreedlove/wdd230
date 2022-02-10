5e0433388f48655826509c3630e32d0936c9cf93
jessicabreedlove <jessicabreedlove@gmail.com>
2022-02-10 15:18:28 -0700

    some progress

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,3 +1,5 @@
+//Hamburger Button
+
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -11,7 +13,7 @@ lastmod.textContent = document.lastModified;
 
 const datefield = document.querySelector(".date");
 
-// derive the current date using a date object
+//Derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
   now
@@ -19,17 +21,20 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
   dateStyle: "full",
 }).format(now);
-// long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
-/*banner JS */
-const day = current.getDay();
-const banner = document.querySelector("#banner");
-console.log(day);
-if (day == 6 || day == 7) {
-  banner.classList.add("show");
+//Banner display date
+
+const today = new Date();
+let dayOfWeek;
+dayOfWeek = today.getDay();
+let banner;
+if (dayOfWeek >= 1 && dayOfWeek <= 2) {
+  banner =
+    ":handshake::skin-tone-3: Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 } else {
-  banner.classList.add("hide");
+  banner = "";
 }
+document.querySelector("#banner").textContent = banner;
