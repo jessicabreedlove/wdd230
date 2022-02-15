98fda18bdb7829c09bffa15d9d6b603d17a7de12
jessicabreedlove <jessicabreedlove@gmail.com>
2022-02-14 22:22:36 -0700

    fixed banner!

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,7 +1,31 @@
-/*Join chamber code*/
 const day = now.getDay();
 
-if (day === 5 || day === 6) {
-  document.querySelector("#joinchamber").innerHTML =
-    "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00pm.";
+switch (day) {
+  case 0:
+    document.querySelector("#banner").style.display = "none";
+    console.log("Sunday");
+    break;
+  case 1:
+    document.querySelector("#banner").style.display = "block";
+    console.log("Monday");
+    break;
+  case 2:
+    document.querySelector("#banner").style.display = "block";
+    console.log("Tuesday");
+    break;
+  case 3:
+    document.querySelector("#banner").style.display = "none";
+    console.log("Wednesday");
+    break;
+  case 4:
+    document.querySelector("#banner").style.display = "none";
+    console.log("Thursday");
+    break;
+  case 5:
+    document.querySelector("#banner").style.display = "none";
+    console.log("Friday");
+    break;
+  case 6:
+    document.querySelector("#banner").style.display = "none";
+    console.log("Saturday");
 }
