645f19f36f94db646a703ea39f7e8255cb5804c9
jessicabreedlove <jessicabreedlove@gmail.com>
2022-02-14 17:11:59 -0700

    windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,21 +1,44 @@
+// let t = parseFloat(document.querySelector("#t").textContent);
+// let s = parseFloat(document.querySelector("#s").textContent);
+// let windchill;
+
+// if (t <= 10 && s > 4.8) {
+//   windchill = windChill(t, s);
+//   windchill = `${windchill}&#176;C`;
+// } else {
+//   windchill = "N/A";
+// }
+// // output
+// document.querySelector("#w").innerHTML = windchill;
+
+// function windChill(tempF, speed) {
+//   windchill =
+//     35.74 +
+//     0.6215 * tempF -
+//     35.75 * Math.pow(speed, 0.16) +
+//     0.4275 * tempF * Math.pow(speed, 0.16);
+//   return windchill.toFixed(2);
+// }
+
 let t = parseFloat(document.querySelector("#t").textContent);
 let s = parseFloat(document.querySelector("#s").textContent);
-let windchill;
+let windchill = "";
 
-if (t <= 10 && s > 4.8) {
+if (t <= 50 && s >= 3) {
   windchill = windChill(t, s);
-  windchill = `${windchill}&#176;C`;
+  windchill = `${windchill}&#176;F`;
 } else {
   windchill = "N/A";
 }
-// output
+
+//  The output
 document.querySelector("#w").innerHTML = windchill;
 
-function windChill(tempF, speed) {
+function windChill(t, s) {
   windchill =
     35.74 +
-    0.6215 * tempF -
-    35.75 * Math.pow(speed, 0.16) +
-    0.4275 * tempF * Math.pow(speed, 0.16);
-  return windchill.toFixed(2);
+    0.6215 * t -
+    35.75 * Math.pow(s, 0.16) +
+    0.4275 * t * Math.pow(s, 0.16);
+  return windchill.toFixed(0);
 }
