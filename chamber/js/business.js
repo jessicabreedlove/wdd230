552a0850ea53914499fc36a06ffda6c71d7b9612
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-03 15:58:36 -0700

    updated json and business js

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -11,3 +11,29 @@ fetch(requestURL)
     const business = jsonObject["business"];
     business.forEach(displayBusiness);
   });
+
+function displayBusiness(business) {
+  // Create elements to add to the document
+  let card = document.createElement("section");
+  let h2 = document.createElement("h2");
+  let p = document.createElement("p");
+  // let logo = document.createElement("img");
+
+  h2.textContent = `${business.name}`;
+  p.innerHTML = `${business.address}<br/>
+    ${business.city}, ${business.state} ${business.zip}<br/>
+    ${business.website}`;
+
+  // Build the image attributes
+  // logo.setAttribute("src", business.imageurl);
+  // logo.setAttribute("alt", `logo of ${business.name}}`);
+  // logo.setAttribute("loading", "lazy");
+
+  // Add/append the section(card) with the h2 element
+  // card.appendChild(logo);
+  card.appendChild(h2);
+  card.appendChild(p);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector("div.cards").appendChild(card);
+}
