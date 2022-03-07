50e08f44148e394c483c243c23498ff95a923b5c
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-07 14:43:43 -0700

    api update - again

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,6 @@
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid={f3c723af6991221a76f6d1976bbfd2c7}";
-// "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f3c723af6991221a76f6d1976bbfd2c7";
+// "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={f3c723af6991221a76f6d1976bbfd2c7}";
 
 fetch(apiURL)
   .then((response) => response.json())
