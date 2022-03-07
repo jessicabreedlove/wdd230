e896b5167d3ea27087fefadb18de65e7bb1ff76b
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-07 13:47:16 -0700

    Began weather API

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,9 @@
+const apiURL =
+  // "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid={f3c723af6991221a76f6d1976bbfd2c7}";
+  "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f3c723af6991221a76f6d1976bbfd2c7";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
