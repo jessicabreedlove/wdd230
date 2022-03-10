704fb91971d464ea644e4c94039a7f123b55bd8f
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-10 16:42:54 -0700

    added wether API

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,44 +1,49 @@
-// let t = parseFloat(document.querySelector("#t").textContent);
-// let s = parseFloat(document.querySelector("#s").textContent);
-// let windchill;
+/*
+Pleasant Grove Utah Weather info-- API
+"id": 5779816,
+        "name": "Pleasant Grove",
+        "state": "UT",
+        "country": "US",
+        "coord": {
+            "lon": -111.738541,
+            "lat": 40.36412
+*/
 
-// if (t <= 10 && s > 4.8) {
-//   windchill = windChill(t, s);
-//   windchill = `${windchill}&#176;C`;
-// } else {
-//   windchill = "N/A";
-// }
-// // output
-// document.querySelector("#w").innerHTML = windchill;
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5779816&appid=f3c723af6991221a76f6d1976bbfd2c7&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const temp = (document.querySelector("#temp").textContent =
+      jsObject.main.temp.toFixed(1));
 
-// function windChill(tempF, speed) {
-//   windchill =
-//     35.74 +
-//     0.6215 * tempF -
-//     35.75 * Math.pow(speed, 0.16) +
-//     0.4275 * tempF * Math.pow(speed, 0.16);
-//   return windchill.toFixed(2);
-// }
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    const wspeed = jsObject.wind.speed;
+    const humid = jsObject.main.humidity;
+    document.querySelector("#weathericon").setAttribute("src", iconsrc);
+    document.querySelector("#weathericon").setAttribute("alt", desc);
+    document.querySelector("figcaption").textContent = desc;
+    document.querySelector("#speed").textContent = wspeed;
+    document.querySelector("#humid").textContent = humid;
+    let wchill = "";
 
-let t = parseFloat(document.querySelector("#t").textContent);
-let s = parseFloat(document.querySelector("#s").textContent);
-let windchill = "";
+    if (temp <= 50 && wspeed > 3) {
+      wchill = windchill(temp, wspeed);
+      wchill = `${wchill}&deg;F`;
+    } else {
+      wchill = "N/A";
+    }
 
-if (t <= 50 && s >= 3) {
-  windchill = windChill(t, s);
-  windchill = `${windchill}&#176;F`;
-} else {
-  windchill = "N/A";
-}
+    document.querySelector("#wchill").innerHTML = wchill;
 
-//  The output
-document.querySelector("#w").innerHTML = windchill;
-
-function windChill(t, s) {
-  windchill =
-    35.74 +
-    0.6215 * t -
-    35.75 * Math.pow(s, 0.16) +
-    0.4275 * t * Math.pow(s, 0.16);
-  return windchill.toFixed(0);
-}
+    function windchill(temp, wspeed) {
+      windchill =
+        35.74 +
+        0.6215 * temp -
+        35.75 * Math.pow(wspeed, 0.16) +
+        0.4275 * temp * Math.pow(wspeed, 0.16);
+      return windchill.toFixed(1);
+    }
+  });
