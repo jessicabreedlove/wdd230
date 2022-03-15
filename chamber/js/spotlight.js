0a381df4ff9acdb27fd94f183ec68e7c016c6f59
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-15 12:42:59 -0600

    random spotlight working

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -11,18 +11,19 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const spotlight = jsonObject["business"];
     // spotlight.forEach(displaySpotlight);
-    randomNum = getRandomInt(0, 11);
-    displaySpotlight(spotlight[0]);
-    displaySpotlight(spotlight[1]);
-    displaySpotlight(spotlight[2]);
+    let random1 = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
+    let random2 = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
+    let random3 = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
+    if (random1 !== random2 && random2 !== random3) {
+      displaySpotlight(spotlight[random1]);
+      displaySpotlight(spotlight[random2]);
+      displaySpotlight(spotlight[random3]);
+    } else {
+      displaySpotlight(spotlight[0]);
+      displaySpotlight(spotlight[1]);
+      displaySpotlight(spotlight[2]);
+    }
   });
-
-//generate a random number
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.floor() * (max - min + 1) + min);
-}
 
 function displaySpotlight(spotlight) {
   let div1 = document.createElement("div");
