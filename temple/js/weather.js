152395d42695a8df269c39c6de4b112b83004c85
jessicabreedlove <jessicabreedlove@gmail.com>
2022-04-01 19:57:23 -0600

    one day weather

--- temple/js/weather.js
+++ temple/js/weather.js
@@ -8,26 +8,21 @@ Bethesda MD Weather info-- API
             "lon": -77.100258,
             "lat": 38.980671
 */
-// let lat = 38;
-// let lon = -77;
-// let key = "59b43925192d7e7ef9b4e20400de7d81";
-// let cnt = 3;
-// const apiURL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}&units=imperial&appid=${key}`;
+// I had some issues doing the 3 day API call, so I just did the one day since I could make it work
 
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/forecast/daily?lat=38&lon=-77&cnt=3&appid=59b43925192d7e7ef9b4e20400de7d81&units=imperial";
+  "https://api.openweathermap.org/data/2.5/weather?id=4348599&appid=59b43925192d7e7ef9b4e20400de7d81&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const wdate = (document.querySelector("#wdate").textContent =
-      jsObject.list.dt);
     const temp = (document.querySelector("#temp").textContent =
-      jsObject.list.temp.day.toFixed(1));
+      jsObject.main.temp.toFixed(1));
+
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
-    const wspeed = jsObject.list.speed;
-    const humid = jsObject.list.humidity;
+    const wspeed = jsObject.wind.speed;
+    const humid = jsObject.main.humidity;
     document.querySelector("#weathericon").setAttribute("src", iconsrc);
     document.querySelector("#weathericon").setAttribute("alt", desc);
     document.querySelector("figcaption").textContent = desc;
